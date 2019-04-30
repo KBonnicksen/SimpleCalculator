839dca821bc6bbd6a254315e916be7ca3ed70705
KELSEY Bonnicksen <bonnicksen8017@students.cptc.edu>
2019-04-30 12:44:26 -0700

    Add project files.

---
+++ SimpleCalculator/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace SimpleCalculator
+{
+    partial class FrmSimpleCalc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOperand1 = new System.Windows.Forms.TextBox();
+            this.txtOperator = new System.Windows.Forms.TextBox();
+            this.txtOperand2 = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtOperand1
+            // 
+            this.txtOperand1.Location = new System.Drawing.Point(173, 89);
+            this.txtOperand1.Name = "txtOperand1";
+            this.txtOperand1.Size = new System.Drawing.Size(100, 20);
+            this.txtOperand1.TabIndex = 0;
+            // 
+            // txtOperator
+            // 
+            this.txtOperator.Location = new System.Drawing.Point(173, 128);
+            this.txtOperator.MaxLength = 1;
+            this.txtOperator.Name = "txtOperator";
+            this.txtOperator.Size = new System.Drawing.Size(28, 20);
+            this.txtOperator.TabIndex = 1;
+            // 
+            // txtOperand2
+            // 
+            this.txtOperand2.Location = new System.Drawing.Point(173, 165);
+            this.txtOperand2.Name = "txtOperand2";
+            this.txtOperand2.Size = new System.Drawing.Size(100, 20);
+            this.txtOperand2.TabIndex = 2;
+            this.txtOperand2.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(173, 205);
+            this.txtResult.MaxLength = 324632323;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+            this.txtResult.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Operand 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Operator:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Operand 2:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(56, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Result:";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(56, 280);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 8;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(173, 280);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // FrmSimpleCalc
+            // 
+            this.AcceptButton = this.btnCalc;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(382, 328);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtOperand2);
+            this.Controls.Add(this.txtOperator);
+            this.Controls.Add(this.txtOperand1);
+            this.Name = "FrmSimpleCalc";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOperand1;
+        private System.Windows.Forms.TextBox txtOperator;
+        private System.Windows.Forms.TextBox txtOperand2;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ SimpleCalculator/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class FrmSimpleCalc : Form
+    {
+        public FrmSimpleCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            if (IsValidData())
+            {
+                string operator1 = txtOperator.Text;
+                decimal num1 = Convert.ToDecimal(txtOperand1.Text);
+                decimal num2 = Convert.ToDecimal(txtOperand2.Text);
+
+                decimal solution = Calculate(num1, operator1, num2);
+                txtResult.Text = Convert.ToString(solution);
+            }
+        }
+
+        private bool IsValidData()
+        {
+            if (isDecimal(txtOperand1) &&
+                isDecimal(txtOperand2) &&
+                IsOperator(txtOperator))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool isPresent(Textbox box)
+        {
+            if (string.IsNullOrWhiteSpace(box.Text))
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+        private bool isDecimal(TextBox box)
+        {
+            try
+            {
+                Convert.ToDecimal(box.Text);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private bool IsOperator(TextBox box)
+        {
+            string validOperators = "/*+-";
+            if (validOperators.Contains(box.Text))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private decimal Calculate(decimal operand1, String operator1, decimal operand2)
+        {
+            switch (operator1)
+            {
+                case "+":
+                    return operand1 + operand2;
+                case "-":
+                    return operand1 - operand2;
+                case "*":
+                    return operand1 * operand2;
+                case "/":
+                    return operand1 / operand2;
+            }
+            return 0;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
